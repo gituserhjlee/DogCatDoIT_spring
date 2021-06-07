cb43c3879c0930539ebf6e2cd77f9d0f7e952a8c
LDH9327 <dlarkfl2@gmail.com>
2021-06-07 10:13:08 +0900

    테스트 메소드 추가

--- DogCatDoIT2/src/main/java/com/pet/app/common/ShopUtil.java
+++ DogCatDoIT2/src/main/java/com/pet/app/common/ShopUtil.java
@@ -7,24 +7,40 @@ import com.pet.app.member.Member;
 @Service("orderUtil")
 public class ShopUtil {
 	/**
-	 * 전화번호와 주소 형식 변환 메소드
-	 * tel : 'tel1'-'tel2'-'tel3'
-	 * addr : 'addr1' + 'addr2' 
-	 * @param dto 변환하려는 Member 객체
-	 * @return dto 변환된 member 객체
+	 * 전화번호와 주소 형식 변환 메소드 
+	 * tel : 'tel1'-'tel2'-'tel3' 
+	 * addr : 'addr1' + 'addr2'
+	 * 
+	 * @param 	dto 변환하려는 Member 객체
+	 * @return 	dto 변환된 member 객체
 	 */
 	public Member transformTelAddr(Member dto) {
-		if(dto==null)
+		if (dto == null)
 			return null;
-		
+
 		String tel = dto.getTel();
 		dto.setTel1(tel.substring(0, tel.indexOf("-")));
-		dto.setTel2(tel.substring(tel.indexOf("-")+1, tel.lastIndexOf("-")));
-		dto.setTel3(tel.substring(tel.lastIndexOf("-")+1));
-		
-		String addr = String.join(" ", dto.getAddr1(),dto.getAddr2());
+		dto.setTel2(tel.substring(tel.indexOf("-") + 1, tel.lastIndexOf("-")));
+		dto.setTel3(tel.substring(tel.lastIndexOf("-") + 1));
+
+		String addr = String.join(" ", dto.getAddr1(), dto.getAddr2());
 		dto.setAddr(addr);
-		
+
 		return dto;
+	}
+	
+	/**
+	 * 할인 적용된 판매가격 반환
+	 * @param discountRate	할인율
+	 * @param itemSalePrice	판매정가
+	 * @return				할인 적용된 판매가
+	 */
+	public long getDiscountedPrice(int discountRate, long itemSalePrice) {
+		return (long) (Math.round((100 - discountRate) / 100.0 * itemSalePrice));
+	}
+	
+	public long getDiscountedPrice(int discountRate, long itemSalePrice, int digit) {
+		long result = (long) (Math.round((100 - discountRate) / 100.0 * itemSalePrice));
+		return result;
 	}
 }

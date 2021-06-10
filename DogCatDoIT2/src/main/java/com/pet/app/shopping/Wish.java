04fa7c76a713d282f6d98eec547bd59ab2f9e3b0
LDH9327 <dlarkfl2@gmail.com>
2021-06-10 13:10:13 +0900

    Wish 클래스 생성

---
+++ DogCatDoIT2/src/main/java/com/pet/app/shopping/Wish.java
@@ -0,0 +1,34 @@
+package com.pet.app.shopping;
+
+public class Wish {
+	private long wishIdx;
+	private long userIdx;
+	private long detailId;
+	private String wish_date;
+	public long getWishIdx() {
+		return wishIdx;
+	}
+	public void setWishIdx(long wishIdx) {
+		this.wishIdx = wishIdx;
+	}
+	public long getUserIdx() {
+		return userIdx;
+	}
+	public void setUserIdx(long userIdx) {
+		this.userIdx = userIdx;
+	}
+	public long getDetailId() {
+		return detailId;
+	}
+	public void setDetailId(long detailId) {
+		this.detailId = detailId;
+	}
+	public String getWish_date() {
+		return wish_date;
+	}
+	public void setWish_date(String wish_date) {
+		this.wish_date = wish_date;
+	}
+	
+	
+}
